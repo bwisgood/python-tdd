44b6a174b7814d27dd0130ced02589670fb7e898
bwisgood <857464370@qq.com>
2019-03-27 16:47:25 +0800

    Modify functional test to use unittest module

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,28 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+# browser = webdriver.Chrome()
+# browser.get('http://localhost:8000')
+#
+# assert 'To-do' in browser.title
+#
+# browser.quit()
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('To-do', self.browser.title)
+        self.fail("Finish test!")
+
+
+if __name__ == '__main__':
+    unittest.main()
