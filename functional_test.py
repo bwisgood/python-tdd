69dd42a9fea7b4f45750317412f4b33c359c0904
bwisgood <857464370@qq.com>
2019-03-28 15:52:18 +0800

    add functional test

--- functional_test.py
+++ functional_test.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 
 # browser = webdriver.Chrome()
@@ -21,6 +23,30 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
 
         self.assertIn('To-do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn("To-Do", header_text)
+
+        # 邀请她写一个待办事项
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            input_box.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # 然后她在文本框中输入了"Buy peacock feathers"
+        input_box.send_keys("Buy peacock feathers")
+
+        # 她按下回车后，待办事项更新了
+        # 待办事项的表格中显示了"1:Buy peacock feathers"
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(2)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1:Buy peacock feathers to make a fly' for row in rows)
+        )
+
         self.fail("Finish test!")
 
 
